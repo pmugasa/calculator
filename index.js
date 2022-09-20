88c2f920fc607444037b326d21611a298eda52ab
Peterson <pmugasa78@gmail.com>
2022-09-20 23:37:49 +0200

    Added basic math functions, add, subtract, multiply and divide

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+function add (a, b){
+return a + b
+}
+console.log(add(2,2))
+function subtract(a,b){
+    return a - b
+}
+console.log(subtract(2,2))
+function multiply(a,b){
+    return a * b
+}
+console.log(multiply(2,2))
+function divide(a, b){
+    return a/b
+}
+console.log(divide(2,2))
+
